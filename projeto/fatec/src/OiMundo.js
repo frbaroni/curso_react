1565ca15d252911edc422386b850f587544fda55
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-10-18 20:34:43 -0200

    Adicionado exemplo de JSX Prop

---
+++ projeto/fatec/src/OiMundo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class OiMundo extends Component {
+  render() {
+    return (
+      <span>{this.props.nomeee}</span>
+    );
+  }
+}
+
+export default OiMundo;
